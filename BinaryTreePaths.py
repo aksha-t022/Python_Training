e27619b639d0ce78f045709b44c9e6f24833a22d
Akshat <akshatsunil52@gmail.com>
2026-04-13 09:41:41 +0530

    Add files via upload

---
+++ BinaryTreePaths.py
@@ -0,0 +1,17 @@
+class Solution:
+    def binaryTreePaths(self, root):
+        if not root:
+            return []
+        
+        if not root.left and not root.right:
+            return [str(root.val)]
+        
+        paths = []
+        
+        for path in self.binaryTreePaths(root.left):
+            paths.append(str(root.val) + "->" + path)
+        
+        for path in self.binaryTreePaths(root.right):
+            paths.append(str(root.val) + "->" + path)
+        
+        return paths
